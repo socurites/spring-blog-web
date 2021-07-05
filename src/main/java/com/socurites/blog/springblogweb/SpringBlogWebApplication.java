85b5b1c2af2bd3992d12e732292d0b6d47da8256
Junyi Song <junyisong@this.local>
2021-07-05 09:44:32 +0900

    Initial commit

---
+++ src/main/java/com/socurites/blog/springblogweb/SpringBlogWebApplication.java
@@ -0,0 +1,13 @@
+package com.socurites.blog.springblogweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogWebApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBlogWebApplication.class, args);
+  }
+
+}
