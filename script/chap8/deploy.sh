4adcbc0cb5b0775128d3baf8d3c8ee05e41dfe57
Junyi Song <junyisong@this.local>
2021-07-08 09:59:01 +0900

    .

---
+++ script/chap8/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step1
+PROJECT_NAME=spring-blog-web
+
+cd $REPOSITORY/$PROJECT_NAME/
+
+echo "> Git Pull"
+git pull
+
+echo "> Build..."
+./gradlew build
+
+echo "> Move to $REPOSITORY"
+cd $REPOSITORY
+
+echo "> Copy build output"
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+CURRENT_PID=$(pgrep -f ${PROJECT_NAME}.*.jar)
+echo "> Current App PID: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID"]; then
+  echo "> Current app is not running now"
+else
+  echo "> Killing $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> Deploying new app"
+JAR_NAME=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
