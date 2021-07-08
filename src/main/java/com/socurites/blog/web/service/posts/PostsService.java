9c34eca0be84719a9a0964c3279472ae337fc189
Junyi Song <junyisong@this.local>
2021-07-08 15:49:06 +0900

    test travis ci

--- src/main/java/com/socurites/blog/web/service/posts/PostsService.java
+++ src/main/java/com/socurites/blog/web/service/posts/PostsService.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
 @Service
 @RequiredArgsConstructor
 public class PostsService {
